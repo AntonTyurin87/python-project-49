03bd633b422695fc2bbb341a9e590cb7531f39c6
Антон Тюрин <AntonTyurin87@gmail.com>
2022-11-30 14:52:48 +0300

    Add files brain_games.py, and chenge files brain_games/__pycache__/__init__.cpython-38.pyc, brain_games/scripts/__pycache__/__init__.cpython-38.pyc, brain_games/scripts/__pycache__/brain_games.cpython-38.pyc, poetry.lock, pyproject.toml.

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,2 +1,5 @@
-print ("""poetry run python -m brain_games.scripts.brain_games
-Welcome to the Brain Games!""")
+def main():
+    print('Welcome to the Brain Games!')
+
+if __name__ == '__main__':
+    main()
