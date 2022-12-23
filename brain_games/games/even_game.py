aacd270996ea24f77a4a7a59380e033ae97506b6
Anton Tyurin <AntonTyurin87@gmail.com>
2022-12-23 17:01:52 +0300

    Modified even_game.py becose Function even_game has a Cognitive Complexity of 8 (exceeds 5 allowed).

--- brain_games/games/even_game.py
+++ brain_games/games/even_game.py
@@ -17,21 +17,17 @@ def even_game():
         number = random.randint(1, 100)
         print(f'Question: {number}')
         your_answer = prompt.string('Your answer: ')
-
         if number % 2 == 0:
             correct_answer = 'yes'
         elif number % 2 != 0:
             correct_answer = 'no'
-
         if your_answer == correct_answer:
             counter = counter + 1
             print('Correct!')
-
         else:
             print(f"'{your_answer}' is wrong answer ;(. ", end='')
             print(f"Correct answer was '{correct_answer}'.")
             print(f"Let's try again, {brain_games.acquaintance.name}!")
             break
-
     if counter == 3:
         print(f'Congratulations, {brain_games.acquaintance.name}!')
