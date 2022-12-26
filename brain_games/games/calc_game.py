70c20c3e7c40ddc5fe16f59c7be5a3c5e314e29e
Anton Tyurin <AntonTyurin87@gmail.com>
2022-12-26 19:32:21 +0300

    Trying to fix Function calc_game has a Cognitive Complexity of 6 (exceeds 5 allowed).

--- brain_games/games/calc_game.py
+++ brain_games/games/calc_game.py
@@ -6,12 +6,7 @@ def rules():
     print('''What is the result of the expression?''')
 
 
-# The functionality of the game "Even-Game"
-def calc_game():
-    number_1 = random.randint(1, 100)
-    number_2 = random.randint(1, 100)
-    arithmetic_sign = random.choice(['+', '-', '*'])
-
+def arifmetic(arithmetic_sign):
     if arithmetic_sign == '+':
         correct_ans = number_1 + number_2
     elif arithmetic_sign == '-':
@@ -19,12 +14,22 @@ def calc_game():
     elif arithmetic_sign == '*':
         correct_ans = number_1 * number_2
 
+    return correct_ans
+
+
+# The functionality of the game "Even-Game"
+def calc_game():
+    global number_1, number_2, arithmetic_sign
+    number_1 = random.randint(1, 100)
+    number_2 = random.randint(1, 100)
+    arithmetic_sign = random.choice(['+', '-', '*'])
+
     print(f'Question: {number_1} {arithmetic_sign} {number_2}')
     your_ans = prompt.string('Your answer: ')
 
     if (your_ans[0] == '-' and your_ans[1::].isdigit()) or your_ans.isdigit():
         your_ans = int(your_ans)
-    # elif your_ans.isdigit():
-        # your_ans = int(your_ans)
 
-    return your_ans, correct_ans
+    return your_ans, arifmetic(arithmetic_sign)
+
+calc_game()
