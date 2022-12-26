19e77e32a8437f2e969e77bf18a05bd0fe61adb0
Anton Tyurin <AntonTyurin87@gmail.com>
2022-12-26 18:20:13 +0300

    Trying to fix the Function calc_game has a Cognitive Complexity of 6 (exceeds 5 allowed).

--- brain_games/games/calc_game.py
+++ brain_games/games/calc_game.py
@@ -13,18 +13,18 @@ def calc_game():
     arithmetic_sign = random.choice(['+', '-', '*'])
 
     if arithmetic_sign == '+':
-        correct_answer = number_1 + number_2
+        correct_ans = number_1 + number_2
     elif arithmetic_sign == '-':
-        correct_answer = number_1 - number_2
+        correct_ans = number_1 - number_2
     elif arithmetic_sign == '*':
-        correct_answer = number_1 * number_2
+        correct_ans = number_1 * number_2
 
     print(f'Question: {number_1} {arithmetic_sign} {number_2}')
-    your_answer = prompt.string('Your answer: ')
+    your_ans = prompt.string('Your answer: ')
 
-    if your_answer[0] == '-' and your_answer[1::].isdigit():
-        your_answer = int(your_answer)
-    elif your_answer.isdigit():
-        your_answer = int(your_answer)
+    if (your_ans[0] == '-' and your_ans[1::].isdigit()) or your_ans.isdigit():
+        your_ans = int(your_ans)
+    # elif your_ans.isdigit():
+        # your_ans = int(your_ans)
 
-    return your_answer, correct_answer
+    return your_ans, correct_ans
